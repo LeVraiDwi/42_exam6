68fc23ae86b56f45153eb4616e57fe22e55a50fb
tcosse <tcosse@student.42.fr>
2022-09-12 16:47:03 +0200

    trop bien

--- mini_serv.c
+++ mini_serv.c
@@ -1,257 +1,201 @@
-#include <string.h>
 #include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <sys/socket.h>
+#include <stdbool.h>
+#include <string.h>
 #include <netinet/in.h>
+#include <sys/socket.h>
 #include <sys/select.h>
-#include <fcntl.h>
 
+int SOCK_FD[__FD_SETSIZE];
 
-#define STDERR_FILENO 2
+int CL_ID[__FD_SETSIZE];
+size_t CL_CNT = 0;
 
+fd_set FDSET_ALL = {};
+fd_set FDSET_RD, FDSET_WR;
 
-//struct//
-typedef struct s_client
+char *READY_Q[__FD_SETSIZE] = {};
+char *PENDING_Q[__FD_SETSIZE] = {};
+
+void *ft_memcpy(void *dst, const void *src, size_t n)
 {
-    int id;
-    int fd;
-    char    *buffer;
-    struct s_client *next;
-}       t_client;
+    char *sdst = dst;
+    const char *ssrc = src;
 
-fd_set fd_all;
-fd_set fd_rd;
-fd_set fd_wr;
-
-//struct fin//
-
-char *str_join(char *buf, char *add)
-{
-	char	*newbuf;
-	int		len;
-
-	if (buf == 0)
-		len = 0;
-	else
-		len = strlen(buf);
-	newbuf = malloc(sizeof(*newbuf) * (len + strlen(add) + 1));
-	if (newbuf == 0)
-		return (0);
-	newbuf[0] = 0;
-	if (buf != 0)
-		strcat(newbuf, buf);
-	free(buf);
-	strcat(newbuf, add);
-	return (newbuf);
+    while (n--)
+    {
+        sdst[n] = ssrc[n];
+    }
+    return dst;
 }
 
-void exit_error(char *str, t_client *client_lst)
-{
-    t_client *tmp;
-
-    tmp = client_lst;
-    while (client_lst)
+//retire et libere les socket des queues
+void clear_queues(void){
+    for(size_t i = 1; i <= CL_CNT; i++)
     {
-        tmp = tmp->next;
-        close(client_lst->fd);
-        free(client_lst->buffer);
-        client_lst = tmp;
+        free(READY_Q[SOCK_FD[i]]);
+        free(PENDING_Q[SOCK_FD[i]]);
     }
-    write(STDERR_FILENO, str, strlen(str));
+    return;
+}
+
+//close les socket
+void clear_sockets(void)
+{
+    for(size_t i = 0; i <= CL_CNT; ++i)
+        close(SOCK_FD[i]);
+}
+
+
+//affiche erreur et clean les socket
+void fatal_error(void)
+{
+    clear_queues();
+    clear_sockets();
+    write(2, "Fatal error\n", 12);
     exit(1);
 }
 
-void send_message(char *msg, t_client *client_lst, t_client *sender)
+//reattribu l'espace necessesaire pour la nouvelle chaine de caractere de la queue
+void str_reserve(char **sp, size_t new_size)
 {
-    while (client_lst)
+    bool empty = !*sp;
+    char *tmp = realloc(*sp, new_size);
+    
+    if (!tmp)
+        fatal_error();
+    if (empty)
+    *tmp = 0;
+    *sp = tmp;
+}
+
+//concataine la chaine et de queue avec un nouvelle chaine;
+void str_append(char **dst, char *src)
+{
+    str_reserve(dst, (*dst ? strlen(*dst) : 0) + strlen(src) + 1); //aloue le nouvelle espace pour la chaine
+    strcat(*dst, src); //concataine les 2 chaine
+}
+
+//ajoute a la queue les message s pour chaque utilisateur autre que les sender
+void append_to_queues(char **queue, char *s, size_t sender_pos)
+{
+    size_t i = 0;
+    //ajoute pour chauqe utilisateur un message
+    while (++i != sender_pos)
+        str_append(&queue[SOCK_FD[i]], s);
+    while(++i <= CL_CNT)
+        str_append(&queue[SOCK_FD[i]], s);
+}
+
+//retire un client
+void remove_client(int pos)
+{
+    FD_CLR(SOCK_FD[pos], &FDSET_ALL); //retire de fdset_all le sock_fd a retirer des client
+    close(SOCK_FD[pos]); //ferme sock_fd
+
+    char tmpbuf[128];
+    sprintf(tmpbuf, "server: client %u just left\n", CL_ID[SOCK_FD[pos]]);
+    append_to_queues(READY_Q, tmpbuf, pos); //ajoute au message a send que le client est parti
+    SOCK_FD[pos] = SOCK_FD[CL_CNT--]; //remplace le client par le client suivant et enleve 1 au client count
+}
+
+int main (int argc, char **argv)
+{
+    //verifie que l' on est le bon nombre d'argument
+    if (argc < 2)
     {
-        if (client_lst != sender && FD_ISSET(client_lst->fd, &fd_wr))
-            send(client_lst->fd, msg, strlen(msg), 0);
-        client_lst = client_lst->next;
+        write (2, "Wrong number of arguments\n", 26);
+        return 1;
     }
-}
 
-ssize_t receive_message(t_client *client_lst, t_client *curr_cli)
-{
-    ssize_t size, ret;
-    char buffer[4098];
+    //setup la structure addresse
+    struct sockaddr_in servaddr = {
+        .sin_family = AF_INET,
+        .sin_addr.s_addr = htonl(2130706433), // equivaux a 127.0.0.1
+        .sin_port = htons(atoi(argv[1])) // attribu le port donner par le user
+    };
+    uint32_t last_id = 0;
 
-    size = 0;
-    while ((ret = recv(curr_cli->fd, buffer, 4098, 0)) > 0)
+    SOCK_FD[0] = socket(AF_INET, SOCK_STREAM, 0); //creer mon socket
+    if (SOCK_FD[0] == -1)
+        fatal_error();
+    if (bind(SOCK_FD[0], (void *)&servaddr, sizeof(servaddr)) == -1) //lie le socket a l' adresse definie dans servaddr
+        fatal_error();
+    if (listen(SOCK_FD[0], 32)) //ecoute le socket
+        fatal_error();
+
+    FD_SET(SOCK_FD[0], &FDSET_ALL); //ajout mon socket dans ma liste de socket
+    while (true)
     {
-        size += ret;
-        buffer[ret] = 0;
-        curr_cli->buffer = str_join(curr_cli->buffer, buffer);
-        if (!curr_cli->buffer)
-            exit_error("Fatal Error\n", client_lst);
-    }
-    if (size == 0 && ret == -1)
-        return -1;
-    return size;
-}
+        FDSET_RD = FDSET_WR = FDSET_ALL;
+        if (select(__FD_SETSIZE, &FDSET_RD, &FDSET_WR, NULL, NULL) == -1) //attend qu'un set soit pret en lecture/ecriture
+            fatal_error();
 
-int extract_message(char **buf, char **msg)
-{
-	char	*newbuf;
-	int	i;
-
-	*msg = 0;
-	if (*buf == 0)
-		return (0);
-	i = 0;
-	while ((*buf)[i])
-	{
-		if ((*buf)[i] == '\n')
-		{
-			newbuf = calloc(1, sizeof(*newbuf) * (strlen(*buf + i + 1) + 1));
-			if (newbuf == 0)
-				return (-1);
-			strcpy(newbuf, *buf + i + 1);
-			*msg = *buf;
-			(*msg)[i + 1] = 0;
-			*buf = newbuf;
-			return (1);
-		}
-		i++;
-	}
-	return (0);
-}
-
-t_client    *remove_client(t_client *client_lst, t_client *rmcli)
-{
-    char str[1000];
-    t_client *prev;
-
-    if (client_lst == rmcli)
-        client_lst = rmcli->next;
-    else
-    {
-        prev = client_lst;
-        while (prev->next != rmcli)
+        if (FD_ISSET(SOCK_FD[0], &FDSET_RD)) //si sock_fd[0] est pret a etre lu il y a un nouvelle connexion
         {
-            prev = prev->next;
-            prev->next = rmcli->next;
+            SOCK_FD[++CL_CNT] = accept(SOCK_FD[0], NULL, NULL); //accept une nouvelle connexion
+            if (SOCK_FD[CL_CNT] == -1) 
+                fatal_error();
+            CL_ID[SOCK_FD[CL_CNT]] = last_id++; //j'ajoute la nouvelle connexion a ma liste de client
+            FD_SET(SOCK_FD[CL_CNT], &FDSET_ALL); // ajoute le client a mon set de socket
+
+            char tmpbuf[128];
+            sprintf(tmpbuf, "server: client %u just arrived\n", CL_ID[SOCK_FD[CL_CNT]]); 
+            append_to_queues(READY_Q, tmpbuf, CL_CNT); //ajoute le message d'arriver du client au message a envoyer
         }
-    }
-    sprintf(str, "server: client %d jsut left\n", rmcli->id);
-    send_message(str, client_lst, rmcli);
-    FD_CLR(rmcli->fd, &fd_all);
-    close(rmcli->fd);
-    free(rmcli->buffer);
-    free(rmcli);
-    return (client_lst);
-}
 
-t_client *handle_client(t_client *client_lst)
-{
-    char    *msg, *str;
-    ssize_t ret;
-    t_client *it, *curr_cli;
-
-    it = client_lst;
-    while (it)
-    {
-        curr_cli = it;
-        it = curr_cli->next;
-        if (FD_ISSET(curr_cli->fd, &fd_rd))
+        for (size_t i = 1; i <= CL_CNT; ++i) // on parcourt les client (pas sock_fd[0])
         {
-            ret = receive_message(client_lst, curr_cli);
-            if (ret == 0)
-                client_lst = remove_client(client_lst, curr_cli);
-            else if (ret > 0)
+            if (FD_ISSET(SOCK_FD[i], &FDSET_RD)) //si le client est pret a etre lu
             {
-                while ((ret = extract_message(&curr_cli->buffer, &msg)) == 1)
+                char rdbuf[1024 + 1];
+                const int rd_set = recv(SOCK_FD[i], rdbuf, 1024, 0); //recois le message
+
+                if (rd_set <= 0)
                 {
-                    if (!(str = malloc(sizeof(char) * (20 + strlen(msg)))))
-                        exit_error("Fatal Error\n", client_lst);
-                    sprintf(str, "client %d: %s", curr_cli->id, msg);
-                    send_message(str, client_lst, curr_cli);
-                    free(msg);
-                    free(str);
+                    remove_client(i--); //si le message est null ou erreur se produit retire le client
+                    continue;
                 }
-                if (ret == -1)
-                    exit_error("Fatal Error\n", client_lst);
+                rdbuf[rd_set] = 0; // on null term le buffer
+                append_to_queues(PENDING_Q, rdbuf, i); // et on ajoute le message au message en attente d'envoyer
+
+                char tmpbuf[128];
+                sprintf(tmpbuf, "client %u: ", CL_ID[SOCK_FD[i]]); // on creer l'entete du fichier
+                char *nl;
+
+                for(size_t j = 1; j <= CL_CNT; ++j){ // on envoie le message au autre client
+                    if (i == j)
+                        continue;
+                    while((nl = strstr(PENDING_Q[SOCK_FD[j]], "\n"))) // on decoupe la chaine a chaque \n
+                    {
+                        const size_t len = nl - PENDING_Q[SOCK_FD[j]] + 1; // on determine la longueur de la chaine jusqu'a sont \n
+                        char slice[len + 1];
+                        slice[len] = 0;
+                        
+                        ft_memcpy(slice, PENDING_Q[SOCK_FD[j]], len); //on copie la partie de chaine dans slice;
+                        str_append(&READY_Q[SOCK_FD[j]], tmpbuf);//on ajoute l' entete au message a envoyer
+                        str_append(&READY_Q[SOCK_FD[j]], slice); //on ajoute le message ensuite
+
+                        strcpy(PENDING_Q[SOCK_FD[j]], nl + 1); // remplace la chaine d' origine par la copie du reste de la chaine
+                        str_reserve(&PENDING_Q[SOCK_FD[j]], strlen(PENDING_Q[SOCK_FD[j]]) + 1); //realloue l'espace pour eviter perte memoire
+                    }
+                }
             }
-        }
-    }
-    return (client_lst);
-}
-
-t_client    *add_newclient(t_client *client_lst, int fd, int id)
-{
-    t_client *newcli, *last;
-    char    str[1000];
-
-    newcli = (t_client *)malloc(sizeof(t_client));
-    if (!newcli)
-        exit_error("Fatal Error\n", client_lst);
-    newcli->id = id;
-    newcli->fd = fd;
-    newcli->buffer = NULL;
-    newcli->next = NULL;
-    FD_SET(fd, &fd_all);
-    sprintf(str, "server: client %d just arrived]n", id);
-    send_message(str, client_lst, newcli);
-    if (!client_lst)
-        return (newcli);
-    last = client_lst;
-    while (last->next)
-        last = last->next;
-    last->next = newcli;
-    return (client_lst);
-}
-
-void handle_server(int sockfd)
-{
-    int         connfd, maxfd, client_id;
-    t_client *client_lst;
-
-    FD_ZERO(&fd_all);
-    FD_SET(sockfd, &fd_all);
-    client_lst = NULL;
-    maxfd = sockfd;
-    client_id = 0;
-    while (1)
-    {
-        fd_rd = fd_all;
-        fd_wr = fd_all;
-        if (select(maxfd + 1, &fd_rd, &fd_wr, NULL, NULL) < 0)
-            continue;
-        else if(FD_ISSET(sockfd, &fd_rd))
-        {
-            connfd = accept(sockfd, NULL, NULL);
-            if (connfd >= 0)
+            if (READY_Q[SOCK_FD[i]] && FD_ISSET(SOCK_FD[i], &FDSET_WR)) // si le socket a des message pret a envoyer et qu'il est pret a ecrire
             {
-                client_lst = add_newclient(client_lst, connfd, client_id);
-                maxfd = connfd > maxfd ? connfd : maxfd;
-                ++client_id;
+                const size_t len = strlen(READY_Q[SOCK_FD[i]]); //la taille du message
+                const int wr_ret = send(SOCK_FD[i], READY_Q[SOCK_FD[i]], len, MSG_NOSIGNAL); //envoie du message
+
+                if (wr_ret == -1) //si envoie echoue on retire le client
+                {
+                    remove_client(i--);
+                    continue;
+                }
+                free(READY_Q[SOCK_FD[i]]); //on libere le mesasge envoyer
+                READY_Q[SOCK_FD[i]] = NULL; //on met les message a envoyer a null
             }
         }
-        else
-            client_lst = handle_client(client_lst);
     }
-}
-
-int main(int ac, char **av)
-{
-    int port, sockfd;
-    struct sockaddr_in servaddr;
-
-    if (ac < 2)
-        exit_error("Wrong number of arguments", NULL);
-    port = atoi(av[1]);
-    sockfd = socket(AF_INET, SOCK_STREAM, 0);
-    if (sockfd == -1)
-        exit_error("Fatal Error\n", NULL);
-    bzero(&servaddr, sizeof(servaddr));
-    servaddr.sin_family = AF_INET;
-    servaddr.sin_addr.s_addr = htonl(2130706433);
-    servaddr.sin_port = htons(port);
-    if (bind(sockfd, (const struct sockaddr *)&servaddr, sizeof(servaddr)))
-    exit_error("Fatal Error\n", NULL);
-    if (listen(sockfd, 10) != 0)
-    exit_error("Fatal Error\n", NULL);
-    handle_server(sockfd);
-    return (0);
+    return 0;
 }
